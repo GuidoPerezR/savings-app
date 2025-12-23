c2f3e6f0ae3805f2c79a80693494bb8170273603
Guido Perez Ruiz <guidopr27@hotmail.com>
2025-12-23 16:37:41 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-dvh bg-black justify-center items-center flex text-white">
+        <h1>Savings app</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
