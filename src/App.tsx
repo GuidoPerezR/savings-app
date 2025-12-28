9c966285259284380e5f41786a0e363ee8501661
Guido Perez Ruiz <guidopr27@hotmail.com>
2025-12-27 19:15:42 -0600

    feat: restructure routing in App component to include Public and Protected layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
 import '@fontsource-variable/plus-jakarta-sans/index.css';
 import { Route, Routes } from 'react-router';
-import { HomePage } from './pages/Home.tsx';
-import { Header } from './components/layout/Header.tsx';
-import { Footer } from './components/layout/Footer.tsx';
-import { LoginPage } from './pages/Login.tsx';
+import { HomePage } from '@/pages/Home.tsx';
+import { Header } from '@/components/layout/Header.tsx';
+import { LoginPage } from '@/pages/Login.tsx';
+import { ProtectedRoute } from '@/components/routes/ProtectedRoute.tsx';
+import { Dashboard } from '@/pages/Dashboard.tsx';
+import { PublicLayout } from '@/components/layout/PublicLayout.tsx';
+import { AuthLayout } from '@/components/layout/AuthLayout.tsx';
 
 function App() {
   return (
     <>
       <Header />
+
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
+        {/* Public Routes */}
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
+
+        {/* Protected Routes */}
+        <Route
+          element={
+            <ProtectedRoute>
+              <AuthLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route
+            path="/dashboard"
+            element={
+              <>
+                <Dashboard />
+              </>
+            }
+          />
+        </Route>
       </Routes>
-      <Footer />
     </>
   );
 }
