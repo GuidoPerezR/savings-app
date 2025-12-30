215d8aadaf7efb62410f37a06ab136b1b8790ce9
Guido Perez Ruiz <guidopr27@hotmail.com>
2025-12-29 18:57:52 -0600

    fix: change import statement to use 'import type' for SVGProps in icon components

--- src/components/icons/CartShopping.tsx
+++ src/components/icons/CartShopping.tsx
@@ -1,4 +1,4 @@
-import { SVGProps } from 'react';
+import type { SVGProps } from 'react';
 
 export const CartShopping = (props: SVGProps<SVGSVGElement>) => (
   <svg
--- src/components/icons/Plus.tsx
+++ src/components/icons/Plus.tsx
@@ -1,4 +1,4 @@
-import { SVGProps } from 'react';
+import type { SVGProps } from 'react';
 
 export const PlusIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
