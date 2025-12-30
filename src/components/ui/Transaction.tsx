cc958cb5d14862498223d41e516663d65c2d8bf9
Guido Perez Ruiz <guidopr27@hotmail.com>
2025-12-29 18:58:11 -0600

    feat: update category IDs in CATEGORIES and TRANSACTIONS for consistency; refactor Transaction component to use dynamic icons

--- src/components/ui/Transaction.tsx
+++ src/components/ui/Transaction.tsx
@@ -3,6 +3,14 @@ import type { Transaction as TransactionType } from '@/types/Transactions';
 
 type Props = Omit<TransactionType, 'id'>;
 
+const categories: Record<
+  number,
+  React.ComponentType<React.SVGProps<SVGSVGElement>>
+> = {
+  1: SavesPig,
+  // 2: OtroIcono,
+};
+
 export const Transaction = ({
   categoryId,
   title,
@@ -10,11 +18,12 @@ export const Transaction = ({
   amount,
   type,
 }: Props) => {
-  
   const textColor = {
     earning: 'text-earning',
     spending: 'text-spending',
   };
+
+  const Icon = categories[categoryId];
 
   const amountText = `${type === 'earning' ? '+' : '-'}$${amount}`;
 
@@ -22,7 +31,7 @@ export const Transaction = ({
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-4">
         <div className={`rounded-full bg-zinc-500/50 p-2`}>
-          <SavesPig className="size-6" />
+          {<Icon className="size-6" />}
         </div>
         <div className="flex flex-col">
           <span className="font-bold">{title}</span>
--- src/consts/Categories.ts
+++ src/consts/Categories.ts
@@ -3,13 +3,13 @@ import type { Category } from '@/types/Categories';
 
 export const CATEGORIES: Category[] = [
   { id: 1, name: 'Compras', icon: CartShopping },
-  { id: 1, name: 'Comida', icon: CartShopping },
-  { id: 1, name: 'Transporte', icon: CartShopping },
-  { id: 1, name: 'Hogar', icon: CartShopping },
-  { id: 1, name: 'Ocio', icon: CartShopping },
-  { id: 1, name: 'Salud', icon: CartShopping },
-  { id: 1, name: 'Educación', icon: CartShopping },
-  { id: 1, name: 'Trabajo', icon: CartShopping },
-  { id: 1, name: 'Regalos', icon: CartShopping },
-  { id: 1, name: 'Otros', icon: CartShopping },
+  { id: 2, name: 'Comida', icon: CartShopping },
+  { id: 3, name: 'Transporte', icon: CartShopping },
+  { id: 4, name: 'Hogar', icon: CartShopping },
+  { id: 5, name: 'Ocio', icon: CartShopping },
+  { id: 6, name: 'Salud', icon: CartShopping },
+  { id: 7, name: 'Educación', icon: CartShopping },
+  { id: 8, name: 'Trabajo', icon: CartShopping },
+  { id: 9, name: 'Regalos', icon: CartShopping },
+  { id: 10, name: 'Otros', icon: CartShopping },
 ];
