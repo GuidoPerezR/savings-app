899ca4886ed57a2efa020bd446ae9cdaed2c1420
Guido Perez Ruiz <guidopr27@hotmail.com>
2026-01-19 22:44:20 -0600

    feat: enhance Dashboard component to fetch and display user total amounts

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,8 +3,29 @@ import { DashboardCard } from '@/components/ui/DashboardCard.tsx';
 import { TRANSACTIONS } from '@/mocks/Transactions.ts';
 import { Transaction } from '@/components/ui/Transaction.tsx';
 import { DashboardArticle } from '@/components/ui/DashboardArticle.tsx';
+import { InputData } from '@/components/ui/InputData';
+import { useEffect } from 'react';
+import { useTotalAmountStore } from '@/store/totalAmountStore';
+import { getTotalAmounts } from '@/functions/totalAmounts';
+import { useAuthStore } from '@/store/authStore';
 
 export function Dashboard() {
+  const user = useAuthStore((state) => state.user);
+  const totalAmounts = useTotalAmountStore((state) => state.totalAmounts);
+  const setTotalAmounts = useTotalAmountStore((state) => state.setTotalAmounts);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await getTotalAmounts(user?.id || '');
+        setTotalAmounts(data);
+      } catch (error) {
+        console.error('Error fetching total amounts:', error);
+      }
+    };
+    getData();
+  }, [setTotalAmounts, user?.id]);
+
   return (
     <main className="flex min-h-dvh w-full items-center justify-center bg-dark px-5 pt-32 pb-24 font-jakarta-sans text-light">
       <section className="grid w-full grid-cols-2 gap-6">
@@ -12,9 +33,12 @@ export function Dashboard() {
           <h3 className="text-center font-semibold text-light/80 uppercase">
             Saldo total actual
           </h3>
-          <span className="mt-4 block text-center text-5xl font-bold">
-            $22,345.67
-          </span>
+          <InputData
+            amount={totalAmounts?.current_balance ?? 0}
+            name={'current_balance'}
+            style="lg"
+          />
+
           <div className="mt-4 flex items-center justify-center gap-2">
             <span className="w-fit rounded-lg bg-green-500/10 px-2 text-sm text-earning">
               +2.4%
@@ -23,15 +47,17 @@ export function Dashboard() {
           </div>
         </DashboardArticle>
 
-        {DASHBOARD_CARDS.map(({ title, icon: Icon, amount, color }, index) => (
-          <DashboardCard
-            key={index}
-            title={title}
-            icon={Icon}
-            amount={amount}
-            color={color}
-          />
-        ))}
+        {DASHBOARD_CARDS.map(
+          ({ title, icon: Icon, color, inputName }, index) => (
+            <DashboardCard
+              key={index}
+              title={title}
+              icon={Icon}
+              color={color}
+              inputName={inputName}
+            />
+          ),
+        )}
 
         <DashboardArticle className="col-span-2">
           <h2 className="text-lg font-bold text-light">Actividad Reciente</h2>
