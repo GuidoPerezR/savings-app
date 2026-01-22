69de302f5baa544d1e14b342cd5368e5be8a0787
Guido Perez Ruiz <guidopr27@hotmail.com>
2026-01-22 16:29:15 -0600

    feat: update totalAmountStore to use TotalAmountData type for improved type safety

--- src/store/totalAmountStore.ts
+++ src/store/totalAmountStore.ts
@@ -1,19 +1,9 @@
 import { create } from 'zustand';
-
-export type TotalAmountStore = {
-  id: number;
-  created_at: Date;
-  current_balance: number;
-  total_earnings: number;
-  total_spendings: number;
-  total_invested: number;
-  total_savings: number;
-  user_id: string;
-} | null;
+import type { TotalAmountData } from '@/types/TotalAmounts';
 
 interface TotalAmountsStore {
-  totalAmounts: TotalAmountStore;
-  setTotalAmounts: (totalAmounts: TotalAmountStore) => void;
+  totalAmounts: TotalAmountData;
+  setTotalAmounts: (totalAmounts: TotalAmountData) => void;
 }
 
 export const useTotalAmountStore = create<TotalAmountsStore>((set) => ({
---
+++ src/types/TotalAmounts.ts
@@ -0,0 +1,10 @@
+export type TotalAmountData = {
+  id: number;
+  created_at: Date;
+  current_balance: number;
+  total_earnings: number;
+  total_spendings: number;
+  total_invested: number;
+  total_savings: number;
+  user_id: string;
+} | null;
