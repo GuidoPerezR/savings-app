957fa6b310a18ace93813b2e269b5f9e43bf7592
Guido Perez Ruiz <guidopr27@hotmail.com>
2026-01-28 18:10:30 -0600

    feat: implement useHistory hook for transaction history management

---
+++ src/hooks/useHistory.ts
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { formatDate } from '@/functions/formatDate';
+import { useTransactionsData } from './useTransactionsData';
+
+export const useHistory = () => {
+  const [period, setPeriod] = useState(() => {
+    const now = new Date();
+    return formatDate(now);
+  });
+
+  const { getTransactionsHistoryData } = useTransactionsData();
+
+  const handlePrevPeriod = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    const prevMonthDate = new Date(period.year, period.month - 1, 1);
+    const newPeriod = formatDate(prevMonthDate);
+    setPeriod(newPeriod);
+  };
+
+  const handleNextPeriod = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    const nextMonthDate = new Date(period.year, period.month + 1, 1);
+    const newPeriod = formatDate(nextMonthDate);
+    setPeriod(newPeriod);
+  };
+
+  const promise = getTransactionsHistoryData(period.month, period.year);
+
+  return {
+    period,
+    handleNextPeriod,
+    handlePrevPeriod,
+    promise,
+  };
+};
--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,20 +1,31 @@
-import { TRANSACTIONS } from '@/mocks/Transactions';
-import { Transaction } from '@/components/ui/Transaction';
 import { ArrowLeft } from '@/components/icons/ArrowLeft';
 import { ArrowRight } from '@/components/icons/ArrowRight';
+import { Suspense } from 'react';
+import { HistoryTransactions } from '@/components/ui/HistoryTransactions';
+import { useHistory } from '@/hooks/useHistory';
 
 export const HistoryPage = () => {
+  const { period, handleNextPeriod, handlePrevPeriod, promise } = useHistory();
+
   return (
     <main className="flex min-h-dvh w-full bg-dark px-5 pt-32 pb-24 font-jakarta-sans text-light">
       <section className="w-full">
         <h2 className="text-2xl font-bold">Historial y Analisis</h2>
 
         <nav className="mt-6 flex w-full items-center justify-between">
-          <a href="#" className="rounded-full bg-zinc-600 p-2">
+          <a
+            href="#"
+            className="rounded-full bg-zinc-600 p-2"
+            onClick={handlePrevPeriod}
+          >
             <ArrowLeft className="size-6" />
           </a>
-          <span className="text-lg font-bold">Diciembre de 2025</span>
-          <a href="#" className="rounded-full bg-zinc-600 p-2">
+          <span className="text-lg font-bold">{period.formattedDate}</span>
+          <a
+            href="#"
+            className="rounded-full bg-zinc-600 p-2"
+            onClick={handleNextPeriod}
+          >
             <ArrowRight className="size-6" />
           </a>
         </nav>
@@ -23,19 +34,9 @@ export const HistoryPage = () => {
           <h3 className="text-lg font-bold">Movimientos</h3>
           <div>
             <ul className="mt-4 space-y-4">
-              {TRANSACTIONS.map(
-                ({ id, categoryId, title, date, amount, type }) => (
-                  <li key={id} className="rounded-2xl bg-navbar p-4">
-                    <Transaction
-                      categoryId={categoryId}
-                      title={title}
-                      date={date}
-                      amount={amount}
-                      type={type}
-                    />
-                  </li>
-                ),
-              )}
+              <Suspense fallback={<div>Cargando...</div>}>
+                <HistoryTransactions promise={promise} />
+              </Suspense>
             </ul>
           </div>
         </div>
