7e4d40b735006be25445c90d662617517b38a43f
Guido Perez Ruiz <guidopr27@hotmail.com>
2026-01-28 21:51:11 -0600

    feat: add new icon components and update CATEGORY_ICONS mapping

--- src/consts/Categories.ts
+++ src/consts/Categories.ts
@@ -1,17 +1,25 @@
 import { CartShopping } from '@/components/icons/CartShopping';
 import type { CategoryName } from '@/types/Categories';
 import type { IconType } from '@/types/DashboardCards';
-import { PlusIcon } from '@/components/icons/Plus';
+import { FoodIcon } from '@/components/icons/FoodIcon';
+import { TransportIcon } from '@/components/icons/TransportIcon';
+import { HomeIcon } from '@/components/icons/HomeIcon';
+import { LeisureIcon } from '@/components/icons/LeisureIcon';
+import { HealthIcon } from '@/components/icons/HealthIcon';
+import { EducationIcon } from '@/components/icons/EducationIcon';
+import { WorkIcon } from '@/components/icons/WorkIcon';
+import { GiftIcon } from '@/components/icons/GiftIcon';
+import { OthersIcon } from '@/components/icons/OthersIcon';
 
 export const CATEGORY_ICONS: Record<CategoryName, IconType> = {
   Compras: CartShopping,
-  Comida: PlusIcon,
-  Transporte: CartShopping,
-  Hogar: CartShopping,
-  Ocio: CartShopping,
-  Salud: CartShopping,
-  Educación: CartShopping,
-  Trabajo: CartShopping,
-  Regalos: CartShopping,
-  Otros: CartShopping,
+  Comida: FoodIcon,
+  Transporte: TransportIcon,
+  Hogar: HomeIcon,
+  Ocio: LeisureIcon,
+  Salud: HealthIcon,
+  Educación: EducationIcon,
+  Trabajo: WorkIcon,
+  Regalos: GiftIcon,
+  Otros: OthersIcon,
 };
