7122b23bb7fdea8cbbf6f51597be4d380c98f065
Guido Perez Ruiz <guidopr27@hotmail.com>
2026-01-30 15:25:34 -0600

    feat: add formatAmount function for currency formatting

---
+++ src/functions/formatAmount.ts
@@ -0,0 +1,7 @@
+export const formatAmount = (amount: number) => {
+  return new Intl.NumberFormat('es-MX', {
+    style: 'currency',
+    currency: 'MXN',
+    minimumFractionDigits: 2,
+  }).format(amount);
+};
